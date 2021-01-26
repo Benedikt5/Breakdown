c2b43ba586342e9c1444b5180de213bef1abb0ae
Aleksandr <aleksandr.kazakov@stockinthechannel.com>
2021-01-26 20:24:15 +0000

    Add rest api for transaction search

---
+++ Breakdown.API/Controllers/TransactionsController.cs
@@ -0,0 +1,57 @@
+﻿using Breakdown.API.Models.Transaction;
+using Breakdown.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Breakdown.API.Controllers
+{
+    [ApiController]
+    [Route("expences")]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly BreakdownContext _ctx;
+
+        public TransactionsController(BreakdownContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Filter(DateTime? after, DateTime? before, int? pageNumber, int pageSize = 30, string orderBy = "date_desc")
+        {
+
+            var trans = _ctx.Transactions.AsQueryable();
+            if (after is { })
+                trans = trans.Where(x => x.Date >= after);
+            if (before is { })
+                trans = trans.Where(x => x.Date <= before);
+
+            var sorted = orderBy switch
+            {
+                "date" => trans.OrderBy(x => x.Date),
+                "category" => trans.OrderBy(x => x.Category.Name),
+                "category_desc" => trans.OrderByDescending(x => x.Category.Name),
+                "amount" => trans.OrderBy(x => x.Amount),
+                "amount_desc" => trans.OrderByDescending(x => x.Amount),
+                _ => trans.OrderByDescending(x => x.Date)
+            };
+
+            return Ok(sorted.Skip(((pageNumber ?? 1) - 1) * pageSize).Take(pageSize)
+                .Select(x => new SearchModel
+                {
+                    Amount = x.Amount,
+                    Category = x.Category.Name,
+                    Date = x.Date,
+                    Description = x.Notes,
+                }).ToList());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetDetails(int id)
+        {
+            return Ok(_ctx.Transactions.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
---
+++ Breakdown.API/Models/Transaction/SearchModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Breakdown.API.Models.Transaction
+{
+    public class SearchModel
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
